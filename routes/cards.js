bff36596fb5ed0a9c2ee22fa89cd2216e8343e44
Ann Mesnyankina <a-mesnyankina@mail.ru>
2023-08-11 05:13:34 +0300

    тесты

--- routes/cards.js
+++ routes/cards.js
@@ -15,8 +15,8 @@ router.delete('/:cardId', celebrate({
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().require().min(2).max(30),
-    link: Joi.string().require().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/),
   }),
 }), createCard);
 
