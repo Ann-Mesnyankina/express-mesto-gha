2fe9c533af228b33c5cdf286fe68d61c3a8f9681
Ann Mesnyankina <a-mesnyankina@mail.ru>
2023-08-12 05:00:49 +0300

    тесты

--- app.js
+++ app.js
@@ -48,17 +48,5 @@ app.use('*', (req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
-
 app.use(errors());
 app.listen(PORT);
