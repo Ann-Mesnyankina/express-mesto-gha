31374e73bfa7285f5c674ce8da531769f019c35d
Ann Mesnyankina <a-mesnyankina@mail.ru>
2023-08-12 04:48:31 +0300

    тесты

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
   createUser, login,
 } = require('./controllers/users');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
