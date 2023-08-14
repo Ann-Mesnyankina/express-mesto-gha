8279f77cd1566a11fd0c63206ba2293a76a705b7
Ann Mesnyankina <a-mesnyankina@mail.ru>
2023-08-14 11:54:20 +0300

    тест

--- controllers/users.js
+++ controllers/users.js
@@ -114,7 +114,7 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findUserByCredentials({ email, password }).select('+password')
+  User.findUserByCredentials({ email, password })
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'mesto-secret-key', { expiresIn: '7d' });
       return res.send({ token });
