896fa379cbbdb79f76751d86f6764e7d29a9a55f
Ann Mesnyankina <a-mesnyankina@mail.ru>
2023-08-15 16:16:29 +0300

    тесты

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,12 +13,12 @@ module.exports.getAllCards = (req, res, next) => {
 
 module.exports.deleteCardById = (req, res, next) => {
   Card.findById(req.params.cardId)
+    .orFail()
     .then((card) => {
       if (!card.owner.equals(req.user._id)) {
         throw new ForbiddenError('Не получится удалить чужую карту');
       }
       Card.deleteMany(card)
-        .orFail()
         .then(() => {
           res.send({ message: 'Карта удалена' });
         })
